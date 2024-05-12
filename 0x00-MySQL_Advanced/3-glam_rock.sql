fc14e12365ddded0116ce5c057e0f62b44bebf51
russiantech <chrisjsmez@gmail.com>
2024-05-12 02:56:46 +0000

    coalasce

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- 3-glam_rock.sql
+-- Task 3: Old school band
+
+-- Query to list all bands with Glam rock as their main style, ranked by their longevity
+SELECT band_name, COALESCE(split, 2022) - formed as lifespan FROM metal_bands
+WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
